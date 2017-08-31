ed6cf86887358df7ea6991c87aa98a624eea717f
basil <basil.mawejje@gmail.com>
2017-08-31 10:34:34 +0200

    can add items to cart

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -3,5 +3,19 @@ class Checkout
 
   def initialize
     @items =  {1=> ['tie', 9.25], 2=> ['Sweater', 45.0], 3=> ['skirt', 19.95]}
+    @cart = []
+    add_items_cart
+  end
+
+  def add_items_cart
+    if @items[1] << @cart
+      'tie added to cart'
+    elsif @items[2] << @cart
+      'sweater added to cart'
+    elsif @items[3] << @cart
+      'skirt added to cart'
+    else
+      'item not available in store'
+    end
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,4 +4,8 @@ describe Checkout do
   it 'has items on initialize' do
     expect(subject.items).to eq ({1=> ['tie', 9.25], 2=> ['Sweater', 45.0], 3=> ['skirt', 19.95]})
   end
+
+  it 'can add items to cart' do
+    expect(subject.add_items_cart).to eq 'tie added to cart'
+  end
 end
