0ff8a099a43afd7c23e5ad0698696ae69a022500
basil <basil.mawejje@gmail.com>
2017-08-31 15:42:58 +0200

    add some specs with the help of raoul

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,21 +1,2 @@
 class Checkout
-  attr_accessor :items, :cart, :discount
-
-  def initialize
-    @items =  {1=> ['tie', 9.25], 2=> ['Sweater', 45.0], 3=> ['skirt', 19.95]}
-    @cart = []
-    scan
-  end
-
-  def scan
-    if @cart << @items[1]
-      'tie added to cart'
-    elsif @cart << @items[2]
-      'sweater added to cart'
-    elsif @cart << @items[3]
-      'skirt added to cart'
-    else
-      'item not available in store'
-    end
-  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,11 +1,49 @@
 require './lib/checkout'
 
 describe Checkout do
-  it 'has items on initialize' do
-    expect(subject.items).to eq ({1=> ['tie', 9.25], 2=> ['Sweater', 45.0], 3=> ['skirt', 19.95]})
+  it 'has a list of items for sale' do
+    expect(subject.items).to be_kind_of Array
   end
 
-  it 'can add items to cart' do
-    expect(subject.scan).to eq 'tie added to cart'
+  it 'has a cart object containing scanned items' do
+    expect(subject.items).to be_kind_of Array
+  end
+
+  context '#total_price' do
+    context 'calculates the correct price for items in cart' do
+      xit 'case #1' do
+        subject.scan('001')
+        subject.scan('002')
+        subject.scan('003')
+
+        actual_total_price = subject.total_price
+        expected_total_price = 66.78
+
+        expect(actual_total_price).to eq expected_total_price
+      end
+
+      xit 'case #2' do
+        subject.scan('001')
+        subject.scan('003')
+        subject.scan('001')
+
+        actual_total_price = subject.total_price
+        expected_total_price = 36.95
+
+        expect(actual_total_price).to eq expected_total_price
+      end
+    end
+  end
+
+  context '#scan' do
+    xit 'adds a single item to the cart' do
+      subject.scan('001')
+
+      expected_content = { name: 'tie', price: 9.25, code: '001' }
+      actual_content = subject.cart.first
+
+      expect(subject.cart.length).to eq 1
+      expect(actual_content).to eq expected_content
+    end
   end
 end
