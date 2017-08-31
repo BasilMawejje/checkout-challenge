27367e20e0f7795f1f899b7c9710ccd6b4348f7a
basil <basil.mawejje@gmail.com>
2017-08-31 16:58:59 +0200

    has lit of items on sale

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,2 +1,11 @@
 class Checkout
+  attr_accessor :items, :total_price
+
+  def initialize
+    @items = [{name: 'tie', price: 9.25, code: '001'}, {name: 'sweater', price: 45.00, code: '002'}, {name: 'skirt', price: 19.95, code: '003'}]
+  end
+
+  def scan(id)
+      
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -11,7 +11,7 @@ describe Checkout do
 
   context '#total_price' do
     context 'calculates the correct price for items in cart' do
-      xit 'case #1' do
+      it 'case #1' do
         subject.scan('001')
         subject.scan('002')
         subject.scan('003')
@@ -33,6 +33,18 @@ describe Checkout do
         expect(actual_total_price).to eq expected_total_price
       end
     end
+
+    xit 'case #3' do
+      subject.scan('001')
+      subject.scan('002')
+      subject.scan('001')
+      subject.scan('003')
+
+      actual_total_price = subject.total_price
+      expected_total_price = 73.76
+
+      expect(actual_total_price).to eq expected_total_price
+    end
   end
 
   context '#scan' do
@@ -43,6 +55,18 @@ describe Checkout do
       actual_content = subject.cart.first
 
       expect(subject.cart.length).to eq 1
+      expect(actual_content).to eq expected_content
+    end
+
+    xit 'adds multiple items to the cart' do
+      subject.scan('001')
+      subject.scan('003')
+      subject.scan('001')
+
+      expected_content = [{name: 'tie', price: 9.25, code: '001'}, {name: 'sweater', price: 45.00, code: '002'}, {name: 'skirt', price: 19.95, code: '003'}]
+      actual_content = [{name: 'tie', price: 9.25, code: '001'}, {name: 'sweater', price: 45.00, code: '002'}, {name: 'skirt', price: 19.95, code: '003'}]
+
+      expect(subject.cart.length).to eq 3
       expect(actual_content).to eq expected_content
     end
   end
